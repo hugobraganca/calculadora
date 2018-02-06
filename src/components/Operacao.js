d25fc94993f8af5aba836f52514b2a3da3a0c8d1
Hugo Braganca <hugobraganca94@gmail.com>
2018-02-05 23:59:34 -0300

    Lógica do botão calcular
     - botão calcular efetuando a soma dos dois valores dos inputs.

--- src/components/Operacao.js
+++ src/components/Operacao.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Picker, StyleSheet } from 'react-native';
 
-export default props => (
-    <Picker styles={styles.operacao}>
-        <Picker.item label='Soma' value='soma' />
-        <Picker.item label='Subtração' value='subtracao' />
-    </Picker>
-);
+export default class Operacao extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { operacao: '' }
+    }
+    render() {
+        return(
+            <Picker 
+            styles={styles.operacao}
+            selectedValue={this.state.operacao}
+            onValueChange={op => {this.setState({operacao: op}); }}
+            >
+                <Picker.item label='Soma' value='soma' />
+                <Picker.item label='Subtração' value='subtracao' />
+            </Picker>
+        );
+    }
+}
 
 const styles = StyleSheet.create ({
     operacao: {
